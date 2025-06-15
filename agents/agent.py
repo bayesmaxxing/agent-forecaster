d6006c782c050337f7ce95763f9974a6a2f2f369
bayesmaxxing <bayesmaxxing@gmail.com>
2025-06-15 14:20:53 +0200

    change file setup

--- agents/agent.py
+++ agents/agent.py
@@ -16,11 +16,15 @@ from .utils.history_util import MessageHistory
 from .utils.tool_util import execute_tools
 
 # Set up logging
+LOGS_DIR = "logs"
+os.makedirs(LOGS_DIR, exist_ok=True)
+log_filename = os.path.join(LOGS_DIR, f'agent_{datetime.now().strftime("%Y%m%d_%H%M%S")}.log')
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler(f'agent_{datetime.now().strftime("%Y%m%d_%H%M%S")}.log'),
+        logging.FileHandler(log_filename),
         logging.StreamHandler()
     ]
 )
